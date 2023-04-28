2e5b096177df0b32e8330328a4884ddd454acd44
Daniel Garcia <daes_g@hotmail.com>
2023-04-28 17:18:01 -0500

    creación de 2do superusuario y exploración del proyecto

--- aplicativo/aplicativo/settings.py
+++ aplicativo/aplicativo/settings.py
@@ -83,6 +83,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'aplicativo',
+        # 'HOST': 'localhost',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'data23',
+        #'PORT': 5432
     }
 }
 
